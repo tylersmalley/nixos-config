ae8d1c90f5fbeb0a36d6286be6d572ffe7570c52
tyler <tylersmalley@gmail.com>
2024-01-04 21:34:28 -0800

    wip
    Signed-off-by: tyler <tylersmalley@gmail.com>

---
+++ systems/nixsrv/vaultwarden.enc.nix
@@ -0,0 +1,18 @@
+_:
+
+{
+  services.vaultwarden.config = {
+    # password: BLc5@FhuYph9px@
+    admin_token = "$argon2id$v=19$m=65540,t=3,p=4$M1xfjgzM2fOca8ldaAW6QaacqwmHkQyWNnM1NMBJIaE$YWhvBRfW19Z92NWPVq6sX6bESSpLpLHGEjMAtnIvfAA";
+
+    domain = "https://vault.smalley.ts.net";
+    invitation_org_name = "Smalley Family";
+
+    smtp_host = "smtp.gmail.com";
+    smtp_from = "tylersmalley@gmail.com";
+    smtp_port = 587;
+    smtp_ssl = true;
+    smtp_username = "tylersmalley@gmail.com";
+    smtp_password = "ucba ejlg yixh oxyh ";
+  };
+}
